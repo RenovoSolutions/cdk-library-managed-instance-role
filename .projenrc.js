fb83300954d69de0cd38f51ff6c4e9e1a79c64e6
Brandon Miller <brandon@digital-reboot.com>
2021-08-26 13:43:15 -0400

    chore: project created with projen

--- .projenrc.js
+++ .projenrc.js
@@ -2,12 +2,12 @@ const { AwsCdkConstructLibrary } = require('projen');
 const project = new AwsCdkConstructLibrary({
   author: 'Brandon Miller',
   authorAddress: 'brandon@digital-reboot.com',
-  cdkVersion: '1.95.2',
+  cdkVersion: '1.120.0',
   defaultReleaseBranch: 'main',
   name: 'cdk-managed-instance-role',
   repositoryUrl: 'https://github.com/brandon/cdk-managed-instance-role.git',
 
-  // cdkDependencies: undefined,        /* Which AWS CDK modules (those that start with "@aws-cdk/") does this library require when consumed? */
+  cdkDependencies: ["@aws-cdk/aws-iam"],        /* Which AWS CDK modules (those that start with "@aws-cdk/") does this library require when consumed? */
   // cdkTestDependencies: undefined,    /* AWS CDK modules required for testing. */
   // deps: [],                          /* Runtime dependencies of this module. */
   // description: undefined,            /* The description is just a string that helps people understand the purpose of the package. */
